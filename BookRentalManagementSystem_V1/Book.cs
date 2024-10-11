211f42a9554622ebf0025d2d3aef93904bc0d895
UT01407 <UT01407@UNICOMTIC15>
2024-10-11 14:27:54 +0530

    Add project files.

---
+++ BookRentalManagementSystem_V1/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V1
+{
+    internal class Book
+    {
+        public int BookId;
+        public string Title;
+        public string Author;
+        public decimal RentalPrice;
+
+        public static string TotalBooks;
+
+
+
+        public Book(int bookId, string title, string author, decimal rentalprice)
+        {
+         
+            BookId = bookId;
+            Title = title;
+            Author = author;
+            RentalPrice = rentalprice;
+              
+        }
+
+        public override string ToString()
+        {
+            return $"ID:{BookId},Title:{Title};Author{Author},RentalPrice{RentalPrice}";
+        }
+        public string DisplayBookInfo() 
+        {
+            return ToString();
+        }
+    }
+
+}
